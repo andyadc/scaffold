5ae49e815cb65dd4ca7729ebddeda088ab220b7c
andyadc <andaicheng@live.com>
2016-10-07 10:19:08 +0800

    add method for StringUtil

--- scaffold-util/src/main/java/com/andyadc/scaffold/util/StringUtil.java
+++ scaffold-util/src/main/java/com/andyadc/scaffold/util/StringUtil.java
@@ -4,6 +4,7 @@ package com.andyadc.scaffold.util;
  * A string utility class that manipulates string.
  *
  * @author andaicheng
+ * @version 1.0, 2016-10-07
  */
 public class StringUtil {
 
@@ -59,4 +60,65 @@ public class StringUtil {
     public static boolean isNotBlank(String str) {
         return !isBlank(str);
     }
+
+    /**
+     * Checks if some strings has any blank string.<br>
+     * If strings == null, return true
+     *
+     * @param strings
+     * @return true if any string is blank, otherwise false.
+     */
+    public static boolean isAnyBlank(String... strings) {
+        if (strings == null) {
+            return true;
+        }
+        for (String s : strings) {
+            if (isBlank(s)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks if all the strings is not blank.
+     *
+     * @param strings
+     * @return true if all string is not blank, otherwise false.
+     */
+    public static boolean isAllNotBlank(String... strings) {
+        return !isAnyBlank(strings);
+    }
+
+    /**
+     * <p>
+     * Checks if some strings has any empty string.
+     * </p>
+     * If strings == null, return true
+     *
+     * @param strings
+     * @return true is any string is empty, otherwise false.
+     */
+    public static boolean isAnyEmpty(String... strings) {
+        if (strings == null) {
+            return true;
+        }
+        for (String s : strings) {
+            if (isEmpty(s)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks if all the strings is not empty.
+     *
+     * @param strings
+     * @return true if all string is not empty, otherwise false.
+     */
+    public static boolean isAllNotEmpty(String... strings) {
+        return !isAnyEmpty(strings);
+    }
+
 }
