679baeccf22354ddace382d3c64321a0567473f1
andyadc <andaicheng@live.com>
2016-10-07 09:48:56 +0800

    add utility StringUtil

---
+++ scaffold-util/src/main/java/com/andyadc/scaffold/util/StringUtil.java
@@ -0,0 +1,62 @@
+package com.andyadc.scaffold.util;
+
+/**
+ * A string utility class that manipulates string.
+ *
+ * @author andaicheng
+ */
+public class StringUtil {
+
+    /**
+     * <p>Checks if a String is empty ("") or null.</p>
+     * <p>
+     * <pre>
+     * StringUtil.isEmpty(null)      = true
+     * StringUtil.isEmpty("")        = true
+     * StringUtil.isEmpty(" ")       = false
+     * StringUtil.isEmpty("bob")     = false
+     * StringUtil.isEmpty("  bob  ") = false
+     * </pre>
+     *
+     * @param str the String to check, may be null
+     * @return <code>true</code> if the String is empty or null
+     */
+    public static boolean isEmpty(String str) {
+        return str == null || str.length() == 0;
+    }
+
+    public static boolean isNotEmpty(String str) {
+        return !isEmpty(str);
+    }
+
+    /**
+     * <p>Checks if a String is whitespace, empty ("") or null.</p>
+     * <p>
+     * <pre>
+     * StringUtil.isBlank(null)      = true
+     * StringUtil.isBlank("")        = true
+     * StringUtil.isBlank(" ")       = true
+     * StringUtil.isBlank("bob")     = false
+     * StringUtil.isBlank("  bob  ") = false
+     * </pre>
+     *
+     * @param str the String to check, may be null
+     * @return <code>true</code> if the String is null, empty or whitespace
+     */
+    public static boolean isBlank(String str) {
+        int strLen;
+        if (str == null || (strLen = str.length()) == 0) {
+            return true;
+        }
+        for (int i = 0; i < strLen; i++) {
+            if (!Character.isWhitespace(str.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isNotBlank(String str) {
+        return !isBlank(str);
+    }
+}
