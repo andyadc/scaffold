8d314549fca7f07a9672a228c32585ca4bf37bbf
andaicheng <andaicheng@live.com>
2016-10-12 12:39:39 +0800

    assert

---
+++ scaffold-util/src/main/java/com/andyadc/scaffold/util/Assert.java
@@ -0,0 +1,42 @@
+package com.andyadc.scaffold.util;
+
+/**
+ * 断言工具类
+ *
+ * @author andaicheng
+ */
+public class Assert {
+
+    private Assert() {
+    }
+
+    public static void isTrue(boolean expression, String message) {
+        if (!expression) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    public static void isTrue(boolean expression) {
+        isTrue(expression, "[Assertion failed] - this expression must be true");
+    }
+
+    public static void isNull(Object object, String message) {
+        if (object != null) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    public static void isNull(Object object) {
+        isNull(object, "[Assertion failed] - the object argument must be null");
+    }
+
+    public static void notNull(Object object, String message) {
+        if (object == null) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    public static void notNull(Object object) {
+        notNull(object, "[Assertion failed] - this argument is required; it must not be null");
+    }
+}
