5fd15ca0085b86b73f1bd8b1c067f33972d4fe68
andyadc <andaicheng@live.com>
2016-10-14 22:34:54 +0800

    ReflectUtil

---
+++ scaffold-util/src/main/java/com/andyadc/scaffold/util/reflect/ReflectUtil.java
@@ -0,0 +1,14 @@
+package com.andyadc.scaffold.util.reflect;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author andaicheng
+ */
+public class ReflectUtil {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReflectUtil.class);
+
+
+}
