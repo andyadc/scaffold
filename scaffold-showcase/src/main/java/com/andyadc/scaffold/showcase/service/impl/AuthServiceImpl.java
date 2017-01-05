f6d668b1a9a82ea6a0e1e6384f2d040561a6c874
andaicheng <andaicheng@gmail.com>
2017-01-05 15:00:10 +0800

    javasimon

--- scaffold-showcase/src/main/java/com/andyadc/scaffold/showcase/service/impl/AuthServiceImpl.java
+++ scaffold-showcase/src/main/java/com/andyadc/scaffold/showcase/service/impl/AuthServiceImpl.java
@@ -3,6 +3,7 @@ package com.andyadc.scaffold.showcase.service.impl;
 import com.andyadc.scaffold.showcase.entity.AuthUser;
 import com.andyadc.scaffold.showcase.mapper.AuthUserMapper;
 import com.andyadc.scaffold.showcase.service.AuthService;
+import org.javasimon.aop.Monitored;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
  * @author andaicheng
  * @version 2017/1/4
  */
+@Monitored
 @Service("authService")
 public class AuthServiceImpl implements AuthService {
 
