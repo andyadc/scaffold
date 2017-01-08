231e69277c9d53710da4309bbacc63ab5ccdbecb
andyadc <andaicheng@gmail.com>
2017-01-08 14:24:54 +0800

    NetUtils

---
+++ scaffold-util/src/main/java/com/andyadc/scaffold/util/net/IPUtils.java
@@ -0,0 +1,14 @@
+package com.andyadc.scaffold.util.net;
+
+import java.net.InetAddress;
+
+/**
+ * @author andaicheng
+ * @version 2017/1/8
+ */
+public class IPUtils {
+
+    private static InetAddress inetAddress;
+    private static String localHost;
+
+}
