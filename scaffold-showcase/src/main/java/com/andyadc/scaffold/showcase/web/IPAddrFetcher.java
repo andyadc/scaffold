c84c2cca568a35166a6deccd1d5bc67e9c1e7cbe
andyadc <andaicheng@gmail.com>
2017-01-08 16:51:11 +0800

    IPAddrFetcher

---
+++ scaffold-showcase/src/main/java/com/andyadc/scaffold/showcase/web/IPAddrFetcher.java
@@ -0,0 +1,38 @@
+package com.andyadc.scaffold.showcase.web;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+/**
+ * @author andaicheng
+ * @version 2017/1/8
+ */
+public class IPAddrFetcher {
+
+    /**
+     * 获取客户端IP地址，支持代理服务器
+     */
+    public static String getRemoteIpAddress(HttpServletRequest request) {
+        String ip = "";
+        //匹配大小写，保证无论Nginx如何配置代理参数，系统都能正常获取代理IP
+        Enumeration<?> enumeration = request.getHeaderNames();
+        while (enumeration.hasMoreElements()) {
+            String paraName = (String) enumeration.nextElement();
+            if ("x-forward-for".equalsIgnoreCase(paraName) || "x-forwarded-for".equalsIgnoreCase(paraName)) {
+                ip = request.getHeader(paraName);
+                break;
+            }
+        }
+        String localIP = "127.0.0.1";
+        if ((ip == null) || (ip.length() == 0) || (ip.equalsIgnoreCase(localIP)) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if ((ip == null) || (ip.length() == 0) || (ip.equalsIgnoreCase(localIP)) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if ((ip == null) || (ip.length() == 0) || (ip.equalsIgnoreCase(localIP)) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+}
