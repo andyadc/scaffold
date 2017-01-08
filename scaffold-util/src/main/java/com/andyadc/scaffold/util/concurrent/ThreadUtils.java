7403e8356f50834ee92fec0a976cabd995332971
andyadc <andaicheng@gmail.com>
2017-01-08 17:57:36 +0800

    ThreadUtils

---
+++ scaffold-util/src/main/java/com/andyadc/scaffold/util/concurrent/ThreadUtils.java
@@ -0,0 +1,36 @@
+package com.andyadc.scaffold.util.concurrent;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 线程相关工具类.
+ *
+ * @author andaicheng
+ * @version 2017/1/8
+ */
+public class ThreadUtils {
+
+    /////////// 线程相关功能//////////
+
+    /**
+     * sleep等待, 单位为毫秒, 已捕捉并处理InterruptedException.
+     */
+    public static void sleep(long durationMillis) {
+        try {
+            Thread.sleep(durationMillis);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    /**
+     * sleep等待，已捕捉并处理InterruptedException.
+     */
+    public static void sleep(long duration, TimeUnit unit) {
+        try {
+            Thread.sleep(unit.toMillis(duration));
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
