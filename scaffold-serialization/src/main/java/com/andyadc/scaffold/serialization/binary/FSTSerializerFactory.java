dbe8d719f8c4981f78c777198a66a6ea595fd9b9
andaicheng <andaicheng@gmail.com>
2017-02-13 18:08:27 +0800

    FSTSerializerFactory

--- scaffold-serialization/src/main/java/com/andyadc/scaffold/serialization/binary/FSTSerializerFactory.java
+++ scaffold-serialization/src/main/java/com/andyadc/scaffold/serialization/binary/FSTSerializerFactory.java
@@ -15,6 +15,30 @@ public class FSTSerializerFactory {
     private static ObjectPool<FSTConfiguration> fstConfigurationObjectPool;
     private static boolean usePool = false;
 
+    public static void initialize() {
+        if (usePool) {
+
+        } else {
+            fstConfiguration = createFstConfiguratio();
+        }
+    }
+
+    public static FSTConfiguration createFstConfiguratio() {
+        return FSTConfiguration.createDefaultConfiguration();
+    }
+
+    public static ObjectPool<FSTConfiguration> getDefaultPool() {
+        return fstConfigurationObjectPool;
+    }
+
+    public static FSTConfiguration getDefaultFstConfiguration() {
+        if (usePool) {
+            return getFstConfiguration(fstConfigurationObjectPool);
+        } else {
+            return fstConfiguration;
+        }
+    }
+
     public static FSTConfiguration getFstConfiguration(ObjectPool<FSTConfiguration> pool) {
         FSTConfiguration fstConfiguration = null;
         try {
