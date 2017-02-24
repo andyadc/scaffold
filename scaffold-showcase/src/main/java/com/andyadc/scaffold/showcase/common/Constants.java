83f85c6cb3eb36bb4b3185db0b6f87bd90bd11c5
andaicheng <andaicheng@gmail.com>
2017-02-24 09:33:48 +0800

    Constants

--- scaffold-showcase/src/main/java/com/andyadc/scaffold/showcase/common/Constants.java
+++ scaffold-showcase/src/main/java/com/andyadc/scaffold/showcase/common/Constants.java
@@ -5,4 +5,9 @@ package com.andyadc.scaffold.showcase.common;
  * @version 2017/2/23
  */
 public class Constants {
+
+    public static final String UTF_8 = "UTF-8";
+
+    private Constants() {
+    }
 }
