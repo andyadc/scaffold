71108c4fc7d0e205337f702d2e55fd21a0960a5c
andaicheng <andaicheng@gmail.com>
2017-03-01 17:58:35 +0800

    RedisCacheTest

---
+++ scaffold-showcase/src/test/java/com/andyadc/scaffold/showcase/test/RedisCacheTest.java
@@ -0,0 +1,42 @@
+package com.andyadc.scaffold.showcase.test;
+
+import com.andyadc.scaffold.showcase.cache.RedisCache;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author andaicheng
+ * @version 2017/3/1
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/applicationContext.xml"})
+public class RedisCacheTest {
+
+    @Autowired
+    private RedisCache redisCache;
+
+    @Test
+    public void testAdd() {
+        redisCache.put("adc", "567", 10000);
+    }
+
+    @Test
+    public void testGet() {
+        System.out.println(redisCache.get("adc"));
+    }
+
+    @Before
+    public void before() {
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++");
+    }
+
+    @After
+    public void after() {
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++");
+    }
+}
