b3329f96f56d1e0e294901ddb53ca714868c15e7
andaicheng <andaicheng@gmail.com>
2017-03-03 12:48:46 +0800

    RetryLimitHashedCredentialsMatcher

--- scaffold-showcase/src/main/java/com/andyadc/scaffold/showcase/auth/security/RetryLimitHashedCredentialsMatcher.java
+++ scaffold-showcase/src/main/java/com/andyadc/scaffold/showcase/auth/security/RetryLimitHashedCredentialsMatcher.java
@@ -7,6 +7,8 @@ import org.apache.shiro.authc.ExcessiveAttemptsException;
 import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
 import org.apache.shiro.cache.Cache;
 import org.apache.shiro.cache.CacheManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -14,6 +16,8 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @author andaicheng
  */
 public class RetryLimitHashedCredentialsMatcher extends HashedCredentialsMatcher {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RetryLimitHashedCredentialsMatcher.class);
 
     private AuthService authService;
     private Cache<String, AtomicInteger> passwordRetryCache;
@@ -26,12 +30,14 @@ public class RetryLimitHashedCredentialsMatcher extends HashedCredentialsMatcher
     public boolean doCredentialsMatch(AuthenticationToken token, AuthenticationInfo info) {
         String username = (String) token.getPrincipal();
         AtomicInteger retryCount = passwordRetryCache.get(username);
+        LOG.info("user: {}, retry: {}", username, retryCount);
         if (retryCount == null) {
             retryCount = new AtomicInteger(0);
             passwordRetryCache.put(username, retryCount);
         }
-        if (retryCount.incrementAndGet() > 5) {
+        if (retryCount.incrementAndGet() >= 5) {
             authService.lockAuthUser(username);
+            LOG.info("lock user: {}", username);
             throw new ExcessiveAttemptsException();
         }
         boolean matches = super.doCredentialsMatch(token, info);
