a71b67a2a681a96d3f0881edbc1d00cd3a81cb80
andaicheng <andaicheng@gmail.com>
2017-04-25 13:43:20 +0800

    File utils

---
+++ scaffold-util/src/main/java/com/andyadc/scaffold/util/collection/CollectionUtils.java
@@ -0,0 +1,61 @@
+package com.andyadc.scaffold.util.collection;
+
+import java.util.Collection;
+
+/**
+ * @author andaicheng
+ * @version 2017/4/25
+ */
+public class CollectionUtils {
+
+    /**
+     * <code>CollectionUtils</code> should not normally be instantiated.
+     */
+    private CollectionUtils() {
+    }
+
+    /**
+     * Null-safe check if the specified collection is empty.
+     * <p>
+     * Null returns true.
+     *
+     * @param coll the collection to check, may be null
+     * @return true if empty or null
+     */
+    public static boolean isEmpty(final Collection<?> coll) {
+        return coll == null || coll.isEmpty();
+    }
+
+    /**
+     * Null-safe check if the specified collection is not empty.
+     * <p>
+     * Null returns false.
+     *
+     * @param coll the collection to check, may be null
+     * @return true if non-null and non-empty
+     */
+    public static boolean isNotEmpty(final Collection<?> coll) {
+        return !isEmpty(coll);
+    }
+
+    //-----------------------------------------------------------------------
+
+    /**
+     * Reverses the order of the given array.
+     *
+     * @param array the array to reverse
+     */
+    public static void reverseArray(final Object[] array) {
+        int i = 0;
+        int j = array.length - 1;
+        Object tmp;
+
+        while (j > i) {
+            tmp = array[j];
+            array[j] = array[i];
+            array[i] = tmp;
+            j--;
+            i++;
+        }
+    }
+}
