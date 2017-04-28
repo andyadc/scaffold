77c0a6b97d4c8aa5aad5609110554df2bdd199d0
andaicheng <andaicheng@gmail.com>
2017-04-28 16:15:42 +0800

    scaffold-lock

---
+++ scaffold-lock/src/main/java/com/andyadc/scaffold/lock/Lock.java
@@ -0,0 +1,8 @@
+package com.andyadc.scaffold.lock;
+
+/**
+ * @author andaicheng
+ * @version 2017/4/28
+ */
+public interface Lock {
+}
---
+++ scaffold-lock/src/test/java/com/andyadc/scaffold/lock/test/LockTest.java
@@ -0,0 +1,8 @@
+package com.andyadc.scaffold.lock.test;
+
+/**
+ * @author andaicheng
+ * @version 2017/4/28
+ */
+public class LockTest {
+}
