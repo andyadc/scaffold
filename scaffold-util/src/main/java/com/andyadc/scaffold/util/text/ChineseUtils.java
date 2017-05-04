8635ea7f2bf3f62202769fd913fdc9a13176ab78
andaicheng <andaicheng@gmail.com>
2017-05-04 11:28:50 +0800

    Refactor

--- scaffold-util/src/main/java/com/andyadc/scaffold/util/text/ChineseUtils.java
+++ scaffold-util/src/main/java/com/andyadc/scaffold/util/text/ChineseUtils.java
@@ -1,4 +1,4 @@
-package com.andyadc.scaffold.util;
+package com.andyadc.scaffold.util.text;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -19,15 +19,12 @@ public class ChineseUtils {
      */
     public static boolean isChinese(char c) {
         Character.UnicodeBlock ub = Character.UnicodeBlock.of(c);
-        if (ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS
+        return ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS
                 || ub == Character.UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS
                 || ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A
                 || ub == Character.UnicodeBlock.GENERAL_PUNCTUATION
                 || ub == Character.UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION
-                || ub == Character.UnicodeBlock.HALFWIDTH_AND_FULLWIDTH_FORMS) {
-            return true;
-        }
-        return false;
+                || ub == Character.UnicodeBlock.HALFWIDTH_AND_FULLWIDTH_FORMS;
     }
 
     /**
