dade51035122092404230fb9ee1af75d85c34c54
andaicheng <andaicheng@gmail.com>
2017-06-13 16:40:36 +0800

    Spring Transaction Test

---
+++ scaffold-showcase/src/main/java/com/andyadc/scaffold/showcase/sys/handle/SpringTransactionHandle.java
@@ -0,0 +1,8 @@
+package com.andyadc.scaffold.showcase.sys.handle;
+
+/**
+ * @author andaicheng
+ * @version 2017/6/13
+ */
+public interface SpringTransactionHandle {
+}
---
+++ scaffold-showcase/src/main/java/com/andyadc/scaffold/showcase/sys/handle/impl/SpringTransactionHandleImpl.java
@@ -0,0 +1,12 @@
+package com.andyadc.scaffold.showcase.sys.handle.impl;
+
+import com.andyadc.scaffold.showcase.sys.handle.SpringTransactionHandle;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author andaicheng
+ * @version 2017/6/13
+ */
+@Service
+public class SpringTransactionHandleImpl implements SpringTransactionHandle {
+}
