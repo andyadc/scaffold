e5270e2370981643eba9fcc77ecaa11741e5078f
andaicheng <andaicheng@gmail.com>
2017-06-13 16:25:20 +0800

    Spring Transaction Test

--- scaffold-showcase/src/test/java/com/andyadc/scaffold/showcase/test/SpringTransactionServiceTest.java
+++ scaffold-showcase/src/test/java/com/andyadc/scaffold/showcase/test/SpringTransactionServiceTest.java
@@ -2,6 +2,8 @@ package com.andyadc.scaffold.showcase.test;
 
 import com.andyadc.scaffold.showcase.sys.entity.SpringTransaction;
 import com.andyadc.scaffold.showcase.sys.service.SpringTransactionService;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +24,15 @@ public class SpringTransactionServiceTest {
     @Test
     public void testSave() {
         springTransactionService.save(new SpringTransaction("test"));
+    }
+
+    @Before
+    public void before() {
+        System.out.println("------------------------------------------------");
+    }
+
+    @After
+    public void after() {
+        System.out.println("------------------------------------------------");
     }
 }
