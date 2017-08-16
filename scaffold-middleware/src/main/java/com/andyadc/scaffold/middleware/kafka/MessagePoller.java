46184a51468a84d095a454666800b478cc3c10ca
andyadc <andaicheng@gmail.com>
2017-08-16 13:54:31 +0800

    kafka

--- scaffold-middleware/src/main/java/com/andyadc/scaffold/middleware/kafka/MessagePoller.java
+++ scaffold-middleware/src/main/java/com/andyadc/scaffold/middleware/kafka/MessagePoller.java
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import java.util.Properties;
+import java.util.concurrent.atomic.AtomicLong;
+
 /**
  * @author andy.an
  * @since 2017/8/15
@@ -12,6 +15,10 @@ import org.springframework.beans.factory.InitializingBean;
 public class MessagePoller implements Runnable, InitializingBean, DisposableBean {
 
     private static final Logger LOG = LoggerFactory.getLogger(MessagePoller.class);
+
+    private static AtomicLong tid = new AtomicLong(0);
+
+    private volatile boolean running = true;
 
     @Override
     public void afterPropertiesSet() throws Exception {
@@ -21,10 +28,22 @@ public class MessagePoller implements Runnable, InitializingBean, DisposableBean
     @Override
     public void run() {
 
+        Thread pollerThread = new Thread(this);
+        pollerThread.setName("MessagePoller-" + tid.getAndIncrement());
+        pollerThread.start();
+    }
+
+    private static Properties getDefaultConfig() {
+        Properties props = new Properties();
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        return props;
     }
 
     @Override
     public void destroy() throws Exception {
-
+        this.running = false;
     }
 }
