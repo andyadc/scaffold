5b14acc3b727203ac703c332071a409daa7a9447
andyadc <andaicheng@gmail.com>
2017-08-17 20:03:12 +0800

    rabbitmq

---
+++ scaffold-middleware/src/main/java/com/andyadc/scaffold/middleware/rabbitmq/HelloProducer.java
@@ -0,0 +1,35 @@
+package com.andyadc.scaffold.middleware.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+/**
+ * @author andy.an
+ * @since 2017/8/17
+ */
+public class HelloProducer {
+
+    private static final String QUEUE_NAME = "rabbit.test";
+    private static final String HOST = "localhost";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(HOST);
+        factory.setPort(5672);
+        factory.setUsername("messager");
+        factory.setPassword("messager123");
+
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        String message = "hello rabbit ";
+        //发送消息到队列中
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+
+        //关闭通道和连接
+        channel.close();
+        connection.close();
+    }
+}
