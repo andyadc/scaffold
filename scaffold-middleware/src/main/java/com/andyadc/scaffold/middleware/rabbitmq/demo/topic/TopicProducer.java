08748eb8501c4146b84ff25bc7312dabcc08b957
andyadc <andaicheng@gmail.com>
2017-08-18 10:33:20 +0800

    rabbitmq

---
+++ scaffold-middleware/src/main/java/com/andyadc/scaffold/middleware/rabbitmq/demo/topic/TopicProducer.java
@@ -0,0 +1,48 @@
+package com.andyadc.scaffold.middleware.rabbitmq.demo.topic;
+
+import com.andyadc.scaffold.middleware.rabbitmq.demo.RabbitConst;
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+/**
+ * @author andy.an
+ * @since 2017/8/18
+ */
+public class TopicProducer {
+
+    private static final String EXCHANGE_NAME = "topic_test";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(RabbitConst.SERVER_HOST);
+        factory.setPort(RabbitConst.SERVER_PORT);
+        factory.setUsername("messager");
+        factory.setPassword("messager123");
+
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
+
+        //待发送的消息
+        String[] routingKeys = new String[]{
+                "quick.orange.rabbit",
+                "lazy.orange.elephant",
+                "quick.orange.fox",
+                "lazy.brown.fox",
+                "quick.brown.fox",
+                "quick.orange.male.rabbit",
+                "lazy.orange.male.rabbit"
+        };
+
+        for (String key : routingKeys) {
+            String message = "From " + key + " routingKey' s message!";
+            channel.basicPublish(EXCHANGE_NAME, key, null, message.getBytes());
+        }
+
+        channel.close();
+        connection.close();
+    }
+}
