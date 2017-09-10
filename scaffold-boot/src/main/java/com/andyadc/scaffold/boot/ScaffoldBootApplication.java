1d9332389d2b16ed9d8a4d1444c712eefdc85508
andyadc <andaicheng@gmail.com>
2017-09-10 14:44:45 +0800

    Add sheba

---
+++ scaffold-boot/src/main/java/com/andyadc/scaffold/boot/ScaffoldBootApplication.java
@@ -0,0 +1,16 @@
+package com.andyadc.scaffold.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author andaicheng
+ * @since 2017/9/10
+ */
+@SpringBootApplication
+public class ScaffoldBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScaffoldBootApplication.class, args);
+    }
+}
