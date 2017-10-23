633cd28cb0f45a3e2452916c0ce9b573c4efabe3
andyadc <andaicheng@gmail.com>
2017-10-23 18:10:29 +0800

    spring cloud config

---
+++ scaffold-cloud/cloud-config/src/main/java/com/andyadc/scaffold/cloud/config/ConfigApplication.java
@@ -0,0 +1,18 @@
+package com.andyadc.scaffold.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author andy.an
+ * @since 2017/10/23
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
