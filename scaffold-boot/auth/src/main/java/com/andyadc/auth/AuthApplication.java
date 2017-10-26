92855ad4cd0e9e8f9aeccef21e2fa18ce4ab708d
andyadc <andaicheng@gmail.com>
2017-10-26 17:32:19 +0800

    auth

--- scaffold-boot/auth/src/main/java/com/andyadc/auth/AuthApplication.java
+++ scaffold-boot/auth/src/main/java/com/andyadc/auth/AuthApplication.java
@@ -1,8 +1,16 @@
 package com.andyadc.auth;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author andy.an
  * @since 2017/10/26
  */
+@SpringBootApplication
 public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
 }
