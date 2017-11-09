18cd4bb84100b9f22fe292ee6a51586abaa0b96a
andyadc <andaicheng@gmail.com>
2017-11-09 18:23:11 +0800

    Redis test

--- scaffold-middleware/src/test/java/com/andyadc/scaffold/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * @author andy.an
- * @since 2017/11/8
- */
-package com.andyadc.scaffold;
---
+++ scaffold-middleware/src/test/java/com/andyadc/scaffold/redis/RedisConst.java
@@ -0,0 +1,12 @@
+package com.andyadc.scaffold.redis;
+
+/**
+ * @author andy.an
+ * @since 2017/11/9
+ */
+public class RedisConst {
+
+    public static final String HOST = "ip";
+    public static final int PORT = 6379;
+    public static final String AUTH = "pwd";
+}
