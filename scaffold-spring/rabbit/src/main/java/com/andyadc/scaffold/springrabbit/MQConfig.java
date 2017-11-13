5f0661ba7a7e71773231d06bb867f6e08074cba3
andyadc <andaicheng@gmail.com>
2017-11-13 18:52:19 +0800

    MQConfig

---
+++ scaffold-spring/rabbit/src/main/java/com/andyadc/scaffold/springrabbit/MQConfig.java
@@ -0,0 +1,12 @@
+package com.andyadc.scaffold.springrabbit;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author andy.an
+ * @since 2017/11/13
+ */
+@Configuration
+public class MQConfig {
+
+}
