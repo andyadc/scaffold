8c9189659f72dffd819d6b949ffcede5dddbcbc8
andyadc <andaicheng@gmail.com>
2017-11-15 20:53:05 +0800

    Spring RabbitMQ Consumer

---
+++ scaffold-spring/rabbit-consumer/src/main/java/com/andyadc/scaffold/rabbitconsumer/ConsumerApp.java
@@ -0,0 +1,12 @@
+package com.andyadc.scaffold.rabbitconsumer;
+
+/**
+ * @author andaicheng
+ * @since 2017/11/15
+ */
+public class ConsumerApp {
+
+    public static void main(String[] args) {
+
+    }
+}
