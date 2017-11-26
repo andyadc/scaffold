b9509bd8a5f0f1b79e505764d2e83e0a7bfa17ce
andyadc <andaicheng@gmail.com>
2017-11-26 11:26:21 +0800

    Spring RabbitMQ

--- scaffold-spring/rabbit-consumer/src/main/java/com/andyadc/scaffold/rabbitconsumer/QueueMessageListener.java
+++ scaffold-spring/rabbit-consumer/src/main/java/com/andyadc/scaffold/rabbitconsumer/QueueMessageListener.java
@@ -12,5 +12,10 @@ public class QueueMessageListener implements MessageListener {
     @Override
     public void onMessage(Message message) {
         System.out.println("--------->> " + new String(message.getBody()));
+
+        // auto 情况下, 抛此异常不会重新入队列
+//        throw new AmqpRejectAndDontRequeueException("not requeue");
+        // 消息会被确认
+//        throw new ImmediateAcknowledgeAmqpException("ack");
     }
 }
