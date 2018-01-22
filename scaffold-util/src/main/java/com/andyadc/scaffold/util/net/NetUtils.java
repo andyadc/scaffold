7f9df341bc2d47c858c544de2004d6e0921b2edf
99BILL\andy.an <andy.an@99bill.com>
2018-01-22 13:23:07 +0800

    NetUtils

--- scaffold-util/src/main/java/com/andyadc/scaffold/util/net/NetUtils.java
+++ scaffold-util/src/main/java/com/andyadc/scaffold/util/net/NetUtils.java
@@ -2,7 +2,10 @@ package com.andyadc.scaffold.util.net;
 
 import javax.net.ServerSocketFactory;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.ServerSocket;
+import java.net.SocketException;
+import java.util.Enumeration;
 import java.util.Random;
 
 /**
@@ -16,9 +19,70 @@ public final class NetUtils {
     private static final int PORT_RANGE_MIN = 1024;
     private static final int PORT_RANGE_MAX = 65535;
 
+    /**
+     * Pre-loaded local address
+     */
+    private static InetAddress localAddress;
+
     private static final Random random = new Random();
 
     private NetUtils() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println(NetUtils.getLocalAddress());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(NetUtils.getLocalAddress());
+    }
+
+    static {
+        try {
+            localAddress = getLocalInetAddress();
+        } catch (SocketException e) {
+            throw new RuntimeException("Fail to get local ip.");
+        }
+    }
+
+    /**
+     * Retrieve local address
+     *
+     * @return the string local address
+     */
+    public static String getLocalAddress() {
+        return localAddress.getHostAddress();
+    }
+
+    /**
+     * Retrieve the first validated local ip address(the Public and LAN ip addresses are validated).
+     *
+     * @return the local address
+     * @throws SocketException the socket exception
+     */
+    public static InetAddress getLocalInetAddress() throws SocketException {
+        Enumeration<NetworkInterface> enu = NetworkInterface.getNetworkInterfaces();
+
+        while (enu.hasMoreElements()) {
+            NetworkInterface ni = enu.nextElement();
+            if (ni.isLoopback()) {
+                continue;
+            }
+
+            Enumeration<InetAddress> addressEnu = ni.getInetAddresses();
+            while (addressEnu.hasMoreElements()) {
+                InetAddress address = addressEnu.nextElement();
+
+                // ignores all invalidated addresses
+                if (address.isLinkLocalAddress() || address.isLoopbackAddress() || address.isAnyLocalAddress()) {
+                    continue;
+                }
+
+                return address;
+            }
+        }
+
+        throw new RuntimeException("No validated local address!");
     }
 
     /**
