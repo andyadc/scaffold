e749d1fa995d29c2473f443acfd141d735480056
99BILL\andy.an <andy.an@99bill.com>
2018-03-21 20:09:16 +0800

    Refactor

--- scaffold-cache/src/main/java/com/andyadc/scaffold/cache/Cache.java
+++
@@ -1,7 +0,0 @@
-package com.andyadc.scaffold.cache;
-
-/**
- * @author andaicheng
- */
-public interface Cache {
-}
--- scaffold-cache/src/test/java/com/andyadc/scaffold/cache/test/CacheTest.java
+++
@@ -1,8 +0,0 @@
-package com.andyadc.scaffold.cache.test;
-
-/**
- * @author andaicheng
- * @version 2017/4/28
- */
-public class CacheTest {
-}
